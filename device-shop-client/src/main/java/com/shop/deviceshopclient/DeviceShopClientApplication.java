c6876729421ad4909826ec38692b32a74a882443
Daniele Cocuzza <dcocuzza01@gmail.com>
2023-02-04 11:17:44 +0100

    spring inizializ

---
+++ device-shop-client/src/main/java/com/shop/deviceshopclient/DeviceShopClientApplication.java
@@ -0,0 +1,13 @@
+package com.shop.deviceshopclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceShopClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceShopClientApplication.class, args);
+	}
+
+}
